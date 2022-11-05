d0d2f409cd25ff2358b6ef848c33a18a7d3916bb
daurenabasov <daurenabasov987@gmail.com>
2022-11-05 23:33:40 +0600

    feat: add header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import s from "./Header.module.css";
+import Logo from "../../../Assets/header-logo.png";
+import Dot from "../../../Assets/header-dot.png";
+import Telegram from "../../../Assets/telegram.png";
+import WhatsApp from "../../../Assets/whatsApp.png";
+import { Link } from "react-scroll";
 
 const Header = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <header id={s.header}>
+        <div className={s.header__logo}>
+          <img src={Logo} alt="logo" />
         </div>
-    );
+        <nav className={s.header__nav}>
+          <div className={s.telephone}>
+            <a href="tel: 8-800-700-60-50">8-800-700-60-50 </a>
+            <span>
+              <img src={Dot} alt="" /> Пн-Вс: 9:00 - 21:00
+            </span>
+          </div>
+          <div className={s.social}>
+            <h3>
+              Быстрая связь с нами <br /> в мессенджерах:
+              <div>
+                <a href="https://telegram.com" target="_blank">
+                  <img src={Telegram} alt="" />
+                </a>
+                <a href="https://whatsapp.com" target="_blank">
+                  <img src={WhatsApp} alt="" />
+                </a>
+              </div>
+            </h3>
+          </div>
+
+          <Link
+            className={s.link__btn}
+            to=""
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Заказать консультацию
+          </Link>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default Header;
